1f4d8f28737df54a782d1a1f403dafaa34f8022a
damelys_y <mely.rq@gmail.com>
2017-06-02 16:33:06 -0400

    Cambios asociados al login y sign up

--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -1,3 +1,4 @@
 class Usuario < ActiveRecord::Base
-  attr_accessible :apellido, :area, :cargo, :codigo_empleado, :correo, :gerencia, :nombre, :supervisor, :telefono, :password_digest
+  has_secure_password
+  attr_accessible :apellido, :area, :cargo, :codigo_empleado, :correo, :gerencia, :nombre, :supervisor, :telefono, :password_digest, :password, :password_confirmation, :usuario, :justificacion, :estatus
 end
