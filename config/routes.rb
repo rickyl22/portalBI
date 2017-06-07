34461591264e4eb7f7a9519e5b7590353623ae55
damelys_y <mely.rq@gmail.com>
2017-06-07 16:41:01 -0400

    Se agregan rutas para la parte de login

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
+  resources :roles_privilegios
+  resources :privilegios
+  resources :roles
   resources :usuarios
   resources :menus
-  resources :sessions
+  #resources :sessions
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
   get "home/index"
   get "home/noticias"
   root :to => "home#index" #Pagina de inicio de la App
