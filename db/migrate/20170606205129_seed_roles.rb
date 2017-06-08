214c941d4fd78e41ae89be08923e19e3b5050b82
damelys_y <mely.rq@gmail.com>
2017-06-08 16:07:04 -0400

    Migracion donde se insertan los roles semillas,  se crea migracion para crear tabla de permisos asignados

--- db/migrate/20170606205129_seed_roles.rb
+++ db/migrate/20170606205129_seed_roles.rb
@@ -5,7 +5,7 @@ class SeedRoles < ActiveRecord::Migration[5.1]
     execute "INSERT INTO roles (alias, nombre, descripcion, created_at, updated_at) VALUES ('admin_ind','Administrador de Indicadores','Administrador del modulo de Indicadores', NOW(), NOW())"
     execute "INSERT INTO roles (alias, nombre, descripcion, created_at, updated_at) VALUES ('cons_lid','Consultor Lider','Rol utilizado por el proveedor para administrar los casos', NOW(), NOW())"
     execute "INSERT INTO roles (alias, nombre, descripcion, created_at, updated_at) VALUES ('cons','Consultor','Rol utilizado por el proveedor para realizar los casos', NOW(), NOW())"
-    execute "INSERT INTO roles (alias, nombre, descripcion, created_at, updated_at) VALUES ('cnt','Cliente','Cliente que solicita los casos', NOW(), NOW())"
+    execute "INSERT INTO roles (alias, nombre, descripcion, created_at, updated_at) VALUES ('cli','Cliente','Cliente que solicita los casos', NOW(), NOW())"
 
   end
 end
--- db/migrate/20170607184406_create_permisos_asignados.rb
+++ db/migrate/20170607184406_create_permisos_asignados.rb
@@ -3,6 +3,7 @@ class CreatePermisosAsignados < ActiveRecord::Migration[5.1]
     create_table :permisos_asignados do |t|
       t.belongs_to :privilegio, index: true
       t.belongs_to :role, index: true
+      t.belongs_to :usuario, index: true
       t.integer :recurso #ID del objeto sobre el cual se tiene permisos asignados
 
       t.timestamps
