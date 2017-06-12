349bdf265b9fc22c734ef153da915b570e2d491f
Ricardo <ricardolira48@hotmail.com>
2017-06-12 13:57:03 -0400

    before merge

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  after_action :verify_authorized
-  after_action :verify_policy_scoped
+  #after_action :verify_authorized
+  #after_action :verify_policy_scoped
   include SessionsHelper
-  include Pundit
+  #include Pundit
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,64 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170607140207) do
+ActiveRecord::Schema.define(version: 20170607184406) do
+
+  create_table "casos", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string "act_tabla"
+    t.string "agrup"
+    t.string "altas"
+    t.string "arpu"
+    t.text "campos"
+    t.string "complejidad"
+    t.text "condiciones"
+    t.string "consultor"
+    t.string "especifique"
+    t.date "fech_asig"
+    t.date "fecha_creado"
+    t.date "fecha_req"
+    t.string "fijo"
+    t.string "im"
+    t.string "infosoft"
+    t.string "movil"
+    t.string "otro"
+    t.string "parque"
+    t.date "periodo_desde"
+    t.date "periodo_hasta"
+    t.string "phone"
+    t.string "pre_post"
+    t.string "recargas"
+    t.string "separacion"
+    t.string "status"
+    t.string "tipo_archivo"
+    t.string "tipo_caso"
+    t.string "titulo"
+    t.string "tlv"
+    t.string "tv"
+    t.string "usuario"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "comentarios", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.datetime "fecha"
+    t.text "texto"
+    t.string "autor"
+    t.integer "caso_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "permisos_asignados", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.bigint "privilegio_id"
+    t.bigint "role_id"
+    t.bigint "usuario_id"
+    t.integer "recurso"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["privilegio_id"], name: "index_permisos_asignados_on_privilegio_id"
+    t.index ["role_id"], name: "index_permisos_asignados_on_role_id"
+    t.index ["usuario_id"], name: "index_permisos_asignados_on_usuario_id"
+  end
 
   create_table "privilegios", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "nombre"
@@ -29,8 +86,29 @@ ActiveRecord::Schema.define(version: 20170607140207) do
   end
 
   create_table "roles_privilegios", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string "privilegio_id"
-    t.string "role_id"
+    t.bigint "privilegio_id"
+    t.bigint "role_id"
+    t.integer "alcance"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["privilegio_id"], name: "index_roles_privilegios_on_privilegio_id"
+    t.index ["role_id"], name: "index_roles_privilegios_on_role_id"
+  end
+
+  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string "usuario"
+    t.string "codigo_empleado"
+    t.string "area"
+    t.string "gerencia"
+    t.string "cargo"
+    t.string "correo"
+    t.string "telefono"
+    t.string "nombre"
+    t.string "apellido"
+    t.string "supervisor"
+    t.string "justificacion"
+    t.string "estatus"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
