2664252c15d293626ac9975579aebdc03206a162
damelys_y <mely.rq@gmail.com>
2017-06-15 16:36:50 -0400

    Se aplican las politicas de seguridad al controlador de usuarios

--- app/controllers/usuarios_controller.rb
+++ app/controllers/usuarios_controller.rb
@@ -1,29 +1,30 @@
 class UsuariosController < ApplicationController
   before_action :set_usuario,  only: [:edit, :update, :destroy]
   skip_after_action :verify_policy_scoped
-  skip_after_action :verify_authorized, :only => [:create, :allowed_params]
+  #skip_after_action :verify_authorized#, :only => [:create, :allowed_params]
   #after_action :verify_authorized
   #after_action :verify_authorized, :except => :index, unless: :devise_controller?
   #after_action :verify_authorized, :only => :index
 
   def index
 
-      @usuarios = Usuario.all
-
+    @usuarios = Usuario.all
+    #reset_session
+    p "Usuario d ela sesion index "+session[:usuario_id].inspect
     authorize @usuarios
   end
 
   def create
     @usuario = Usuario.create(allowed_params)
-    p "usuario   "+@usuario.inspect
     authorize @usuario
     @usuario.save
+    p "Usuario d ela sesion create "+session[:usuario_id].inspect
     redirect_to login_path
   end
 
   def new
     @usuario = Usuario.new
-    authorize @usuario
+    #authorize @usuario
   end
 
   def edit
@@ -32,12 +33,13 @@ class UsuariosController < ApplicationController
 
   def show
    @usuario = Usuario.find(params[:id])
+   p "Usuario d ela sesion show "+session[:usuario_id].inspect
    authorize @usuario
   end
 
   def update
     @usuario = Usuario.find(params[:id])
-    authorize @usuario
+    # authorize @usuario
     if @usuario.update_attributes(params[:usuario])
       redirect_to @usuario
     else
@@ -56,11 +58,11 @@ class UsuariosController < ApplicationController
     def set_usuario
       p"ENTRA ACAAA"
       @usurios = Usuario.find(params[:id])
-      authorize @usuarios
+      #authorize @usuarios
     end
 
   def allowed_params
-    params.require(:usuario).permit(:estatus, :usuario, :codigo_empleado, :nombre, :apellido, :correo, :cargo, :area, :supervisor, :gerencia, :telefono, :password_digest, :justificacion, :password, :password_confirmation)
+    params.require(:usuario).permit(:estatus, :role_id, :usuario, :codigo_empleado, :nombre, :apellido, :correo, :cargo, :area, :supervisor, :gerencia, :telefono, :password_digest, :justificacion, :password, :password_confirmation)
   end
 
 end
