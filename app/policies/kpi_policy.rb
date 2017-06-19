606dff921a1b62c3fafa81495d29437716e732ed
Damelys <mely.rq@gmail.com>
2017-06-19 00:25:29 -0400

    modificando metodo rol

--- app/policies/kpi_policy.rb
+++ app/policies/kpi_policy.rb
@@ -5,30 +5,29 @@ class KpiPolicy < ApplicationPolicy
     end
   end
 
-  rol = user.rol?(user.id)
   def index?
     p "usuario- poli -index"
-    return true if user.present? && (rol.alias == "admin" || rol.alias == "admin_ind" || rol.alias == "cli_ind")
+    return true if user.present? && (rol == "admin" || rol == "admin_ind" || rol == "cli_ind")
   end
 
   def create?
     p "usuario- poli -create"
-    return true if user.present? && (rol.alias == "admin" || rol.alias == "admin_ind")
+    return true if user.present? && (rol == "admin" || rol == "admin_ind")
   end
 
   def show?
     p "usuario- poli -show"
-    return true if user.present? && (rol.alias == "admin" || rol.alias == "admin_ind" || rol.alias == "cli_ind")
+    return true if user.present? && (rol == "admin" || rol == "admin_ind" || rol == "cli_ind")
   end
 
   def update?
     p "usuario- poli -update"
-    return true if user.present? && (rol.alias == "admin" || rol.alias == "admin_ind")
+    return true if user.present? && (rol == "admin" || rol == "admin_ind")
   end
 
   def destroy?
     p "usuario- poli -destroy"
-    return true if user.present? && (rol.alias == "admin" || rol.alias == "admin_ind")
+    return true if user.present? && (rol == "admin" || rol == "admin_ind")
   end
 
   private
