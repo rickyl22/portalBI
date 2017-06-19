5b88992fcfe966fefcfa670dc2732c116e26c396
Damelys <mely.rq@gmail.com>
2017-06-19 00:24:16 -0400

    agregando alcance

--- app/policies/usuario_policy.rb
+++ app/policies/usuario_policy.rb
@@ -1,29 +1,47 @@
 class UsuarioPolicy < ApplicationPolicy
-
-  rol = user.rol?(user.id)
-  def index?
+    def index?
     p "usuario- poli -index"
-    return true if user.present? && (rol.alias == "admin" || rol.alias == "admin_ind" || rol.alias == "admin_min")
+    p "EL ROL "+rol.inspect
+    return true if user.present? && (rol == "admin" || rol == "admin_ind" || rol == "admin_min")
   end
 
   def create?
     p "usuario- poli -create"
+    p "EL ROL "+rol.inspect
     true
   end
 
   def show?
     p "usuario- poli -show"
-    return true if user.present? && (rol.alias == "admin" || rol.alias == "admin_ind" || rol.alias == "admin_min")
+    p "EL ROL "+rol.inspect
+    return true if user.present? && (rol == "admin" || rol.alias == "admin_ind" || rol == "admin_min")
   end
 
   def update?
     p "usuario- poli -update"
-    return true if user.present? && (rol.alias == "admin" || rol.alias == "admin_ind" || rol.alias == "admin_min")
+    p "EL ROL "+rol.inspect
+    return true if user.present? && (rol == "admin" || rol == "admin_ind" || rol == "admin_min")
   end
 
   def destroy?
     p "usuario- poli -destroy"
-    return true if user.present? && (rol.alias == "admin" || rol.alias == "admin_ind" || rol.alias == "admin_min")
+    p "EL ROL "+rol.inspect
+    return true if user.present? && (rol == "admin" || rol == "admin_ind" || rol == "admin_min")
+  end
+
+  class Scope < Scope
+    def resolve
+      if admin?
+        p "Scope- es admin"
+        scope.all
+      elsif admin_min?
+        p "Scope- es admin mineria"
+        scope.where("role_id in (?)", "2,4,5,6")
+      elsif admin_ind?
+        p "Scope- es indicadores"
+        scope.where("role_id in (?)", "3,7")
+      end
+    end
   end
 
   private
