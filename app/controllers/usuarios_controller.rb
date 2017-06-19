135a49342b69a855682f9eba2c588c6c3b4a0bda
damelys_y <mely.rq@gmail.com>
2017-06-19 16:31:05 -0400

    Cambio en la vista de login

--- app/controllers/usuarios_controller.rb
+++ app/controllers/usuarios_controller.rb
@@ -1,13 +1,14 @@
 class UsuariosController < ApplicationController
   before_action :set_usuario,  only: [:edit, :update, :destroy]
   after_action :verify_policy_scoped, :except => :create
+  #after_action :verify_policy_scoped#, :except => [:index]
   #skip_after_action :verify_authorized#, :only => [:create, :allowed_params]
   #after_action :verify_authorized
   #after_action :verify_authorized, :except => :index, unless: :devise_controller?
   #after_action :verify_authorized, :only => :index
 
   def index
-
+    p "Entra index"
     @usuarios = Usuario.all
     authorize @usuarios
     @usuarios = policy_scope(Usuario)
@@ -30,6 +31,7 @@ class UsuariosController < ApplicationController
 
   def edit
     @usuario = Usuario.find(params[:id])
+    #@usuario = policy_scope(Usuario).find(params[:id])
   end
 
   def show
@@ -51,15 +53,16 @@ class UsuariosController < ApplicationController
   def destroy
     @usuario = Usuario.find(params[:id])
     @usuario.destroy
-
-    redirect_to usuarios_path_path
+    redirect_to usuarios_path
   end
 
   private
     def set_usuario
-      p"ENTRA ACAAA"
-      @usurio = Usuario.find(params[:id])
+      p"ENTRA ACAAA "+params[:id].inspect
+      @usuario = Usuario.find(params[:id])
+      p "Usiaro "+@usuario.inspect
       authorize @usuario
+      @usuario = policy_scope(Usuario).find(params[:id])
     end
 
   def allowed_params
