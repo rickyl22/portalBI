9a8d44703ad3425a11e3179f9833c6ef3c392fa6
damelys_y <mely.rq@gmail.com>
2017-07-14 14:22:43 -0400

    ...

--- db/migrate/20170616210132_create_kpis.rb
+++ db/migrate/20170616210132_create_kpis.rb
@@ -4,6 +4,7 @@ class CreateKpis < ActiveRecord::Migration[5.1]
       t.text :nombre
       t.text :descripcion
       t.text :portada
+      t.text :dashboard
       t.text :url
 
       t.timestamps
